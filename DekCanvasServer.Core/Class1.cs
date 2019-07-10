e38186f64e907b5dc3945bc5cdd1f760f59d3a4d
timpe <timpe@beijerelectronics.com>
2019-07-10 18:13:24 +0200

    Init

---
+++ DekCanvasServer.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DekCanvasServer.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using DekCanvasServer.Core;
+
+namespace DekCanvasServer
+{
+    class SystemServicesImpl : SystemServices.SystemServicesBase
+    {
+        // Server side handler of the SayHello RPC
+        public override Task<ResizeWindowResponse> ResizeWindow(ResizeWindowRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResizeWindowResponse { });
+        }
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { SystemServices.BindService(new SystemServicesImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+}
